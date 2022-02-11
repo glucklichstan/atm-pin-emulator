2ec5896fa22e3ae4efb1ff502e03cfd4b957a290
glucklichstan <glucklichstas@gmail.com>
2022-02-11 23:58:20 +0200

    added js file

---
+++ script.js
@@ -0,0 +1,20 @@
+// var a = document.getElementById('test');
+// a.style.background = 'red';
+
+// let attemp = 0;
+// let pin = "1111";
+// let count = 3;
+// do {
+//     let pinEnter = ("Enter a PIN-code. You have only " + count + " attempt(s).");
+//     if (pinEnter === pin) {
+//         //("Success!");
+//         break;
+//     } else {
+//         //("Try again");
+//         attemp++;
+//         count--;
+//         if (attemp == 3) {
+//             //("Card is locked! Try to get more information later.");
+//         }
+//     }
+// } while (attemp < 3);
