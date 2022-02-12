c889c1c95e0ae4b39f571765debf445723d05209
glucklichstan <glucklichstas@gmail.com>
2022-02-12 14:35:33 +0200

    changes

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-// var a = document.getElementById('test');
-// a.style.background = 'red';
+//var countingAttempts = document.getElementById('attepmptsId');
+
+document.getElementById('buttonId').onclick = myClick;
+
+function myClick() {
+    let recivePin = document.getElementById('pinId').value;
+    console.log(recivePin);
+}
+
+    
+
 
 // let attemp = 0;
 // let pin = "1111";
-// let count = 3;
+// var count = 3;
+
 // do {
-//     let pinEnter = ("Enter a PIN-code. You have only " + count + " attempt(s).");
+
+//     let pinEnter = 0;
 //     if (pinEnter === pin) {
-//         //("Success!");
+//         countingAttempts.style.color = "red";
 //         break;
 //     } else {
 //         //("Try again");
 //         attemp++;
 //         count--;
+
+//         countingAttempts.textContent = count;
+
 //         if (attemp == 3) {
 //             //("Card is locked! Try to get more information later.");
 //         }
